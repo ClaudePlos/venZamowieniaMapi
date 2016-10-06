7fe2741bc691f35c756435401fc6015c387b9e17
Klaudiusz Skowronski <claude-plos@o2.pl>
2016-10-06 17:46:52 +0200

    k.skowronski jasper3

--- src/java/pl/modelsView/StanZywionychReportsVM.java
+++ src/java/pl/modelsView/StanZywionychReportsVM.java
@@ -5,12 +5,18 @@
  */
 package pl.modelsView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Date;
 import javax.ejb.EJB;
 import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.exporter.pdf.PdfExporter;
+import org.zkoss.util.media.AMedia;
 import org.zkoss.zk.ui.Executions;
+import org.zkoss.zul.Filedownload;
+import org.zkoss.zul.Grid;
 import org.zkoss.zul.Label;
 import org.zkoss.zul.Listbox;
 import org.zkoss.zul.Window;
@@ -28,10 +34,22 @@ public class StanZywionychReportsVM {
     
     @Command
     @NotifyChange("stanyZywionychReprots")
-    public void stanZywionychReports(@BindingParam("naDzien") Date naDzien, @BindingParam("kierKosztow") int kierKosztow){
+    public void stanZywionychReports(@BindingParam("naDzien") Date naDzien, @BindingParam("kierKosztow") int kierKosztow) throws IOException, Exception{
         
-        Window window = (Window)Executions.createComponents(
-                "/jasperreport.zul", null, null);
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+     
+            Grid grid = new Grid();
+            
+            PdfExporter exporter = new PdfExporter();
+            exporter.export(grid, out);
+
+            AMedia amedia = new AMedia("FirstReport.pdf", "pdf", "application/pdf", out.toByteArray());
+            Filedownload.save(amedia);      
+            out.close();
+        
+        
+        /*Window window = (Window)Executions.createComponents(
+                "/orderReports/report_for_plock_hospital.zul", null, null);
         window.doModal();
         
         Listbox listBoxR = new Listbox();
@@ -41,7 +59,7 @@ public class StanZywionychReportsVM {
         test.setValue("Test ks dzis");
         
         window.appendChild(test);
-        window.appendChild(listBoxR);
+        window.appendChild(listBoxR);*/
     
     }
     
