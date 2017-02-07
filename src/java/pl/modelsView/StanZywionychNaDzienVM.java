0ec0a9258f0dc47b9f78ee0dd8718fc7003b5336
ClaudePlos <claude-plos@o2.pl>
2017-02-07 11:54:27 +0100

    confirm order Client (import)

--- src/java/pl/modelsView/StanZywionychNaDzienVM.java
+++ src/java/pl/modelsView/StanZywionychNaDzienVM.java
@@ -270,12 +270,6 @@ public class StanZywionychNaDzienVM extends SelectorComposer<Component> {
 
     }
     
-    @Command
-    @NotifyChange("onClickRowGrid")
-    public void onClickRowGrid(Intbox ds) { 
-        
-        ds.setValue(0);
-    }
 
     @Command
     @NotifyChange("stanyZywionychNaDzien")
