8c621ab2c9133cf6fd8f6d185d3b02ca02a8d3c7
Klaudiusz Skowroński <claude-plos@o2.pl>
2019-12-18 14:52:31 +0100

    new logo and features

--- web/additionalInfo/jadlospisForDietInDay/scripts/jadlospis.js
+++ web/additionalInfo/jadlospisForDietInDay/scripts/jadlospis.js
@@ -65,6 +65,226 @@ function generateDisabilityForWorkersOnSk(parameters){
 	
 	 getInfAboutJadlospisForDiet(parameters, function(error, listJadlospis){
            console.log(listJadlospis);
+           
+           
+          var myTableDiv = document.getElementById("divJadlospisTable");
+	  myTableDiv.innerHTML = ""; //clear table 
+          
+          
+          	//table head
+	  var table = document.createElement('TABLE');
+	  table.border = '0';
+
+	  var tableThead = document.createElement('thead');
+	  table.appendChild(tableThead);
+
+		//HEAD
+	 var trHead = document.createElement('TR');
+	 tableThead.appendChild(trHead);
+	 //01 cell
+	 var tdHead01 = document.createElement('TD');
+			 tdHead01.width = '175';
+			 tdHead01.setAttribute("style", "font-weight:bold");
+			 tdHead01.appendChild(document.createTextNode("Dieta Kod"));
+			 trHead.appendChild(tdHead01);
+	 //02 cell
+	 var tdHead02 = document.createElement('TD');
+			 tdHead02.width = '175';
+			 tdHead02.setAttribute("style", "font-weight:bold");
+			 tdHead02.appendChild(document.createTextNode("Dieta Nazwa"));
+			 trHead.appendChild(tdHead02);
+	 //03 cell
+	 var tdHead = document.createElement('TD');
+			 tdHead.width = '175';
+			 tdHead.setAttribute("style", "font-weight:bold");
+			 tdHead.appendChild(document.createTextNode("Data"));
+			 trHead.appendChild(tdHead);
+
+	 //04 cell
+	 var tdHead = document.createElement('TD');
+			 tdHead.width = '175';
+			 tdHead.setAttribute("style", "font-weight:bold");
+			 tdHead.appendChild(document.createTextNode("w1"));
+			 trHead.appendChild(tdHead);
+
+	 var tdHead = document.createElement('TD');
+			 tdHead.width = '175';
+			 tdHead.setAttribute("style", "font-weight:bold");
+			 tdHead.appendChild(document.createTextNode("w2"));
+			 trHead.appendChild(tdHead);
+
+	 var tdHead = document.createElement('TD');
+ 			tdHead.width = '175';
+ 			tdHead.setAttribute("style", "font-weight:bold");
+ 			tdHead.appendChild(document.createTextNode("w3"));
+			 trHead.appendChild(tdHead);
+			 
+	var tdHead = document.createElement('TD');
+ 			tdHead.width = '175';
+ 			tdHead.setAttribute("style", "font-weight:bold");
+ 			tdHead.appendChild(document.createTextNode("w4"));
+			 trHead.appendChild(tdHead);
+			 
+	var tdHead = document.createElement('TD');
+ 			tdHead.width = '175';
+ 			tdHead.setAttribute("style", "font-weight:bold");
+ 			tdHead.appendChild(document.createTextNode("w5"));
+ 			trHead.appendChild(tdHead);
+                        
+        var tdHead = document.createElement('TD');
+ 			tdHead.width = '175';
+ 			tdHead.setAttribute("style", "font-weight:bold");
+ 			tdHead.appendChild(document.createTextNode("w6"));
+ 			trHead.appendChild(tdHead);
+                        
+        var tdHead = document.createElement('TD');
+ 			tdHead.width = '175';
+ 			tdHead.setAttribute("style", "font-weight:bold");
+ 			tdHead.appendChild(document.createTextNode("w7"));
+ 			trHead.appendChild(tdHead);
+                        
+        var tdHead = document.createElement('TD');
+ 			tdHead.width = '175';
+ 			tdHead.setAttribute("style", "font-weight:bold");
+ 			tdHead.appendChild(document.createTextNode("w8"));
+ 			trHead.appendChild(tdHead);
+                        
+        var tdHead = document.createElement('TD');
+ 			tdHead.width = '175';
+ 			tdHead.setAttribute("style", "font-weight:bold");
+ 			tdHead.appendChild(document.createTextNode("w9"));
+ 			trHead.appendChild(tdHead);
+                        
+        var tdHead = document.createElement('TD');
+ 			tdHead.width = '175';
+ 			tdHead.setAttribute("style", "font-weight:bold");
+ 			tdHead.appendChild(document.createTextNode("w10"));
+ 			trHead.appendChild(tdHead); 
+                        
+        var tdHead = document.createElement('TD');
+ 			tdHead.width = '175';
+ 			tdHead.setAttribute("style", "font-weight:bold");
+ 			tdHead.appendChild(document.createTextNode("w11"));
+ 			trHead.appendChild(tdHead); 
+                        
+        var tdHead = document.createElement('TD');
+ 			tdHead.width = '175';
+ 			tdHead.setAttribute("style", "font-weight:bold");
+ 			tdHead.appendChild(document.createTextNode("w12"));
+ 			trHead.appendChild(tdHead); 
+                        
+        var tdHead = document.createElement('TD');
+ 			tdHead.width = '175';
+ 			tdHead.setAttribute("style", "font-weight:bold");
+ 			tdHead.appendChild(document.createTextNode("w13"));
+ 			trHead.appendChild(tdHead); 
+                        
+        var tdHead = document.createElement('TD');
+ 			tdHead.width = '175';
+ 			tdHead.setAttribute("style", "font-weight:bold");
+ 			tdHead.appendChild(document.createTextNode("w14"));
+ 			trHead.appendChild(tdHead);      
+                        
+        var tdHead = document.createElement('TD');
+ 			tdHead.width = '175';
+ 			tdHead.setAttribute("style", "font-weight:bold");
+ 			tdHead.appendChild(document.createTextNode("w15"));
+ 			trHead.appendChild(tdHead);
+                        
+        var tdHead = document.createElement('TD');
+ 			tdHead.width = '175';
+ 			tdHead.setAttribute("style", "font-weight:bold");
+ 			tdHead.appendChild(document.createTextNode("w16"));
+ 			trHead.appendChild(tdHead);                 
+                        
+        for (var i in listJadlospis) {
+            
+            var tableBody = document.createElement('TBODY');
+            table.appendChild(tableBody);
+            var tr = document.createElement('TR');
+ 			 
+            var td = document.createElement('TD');
+            td.appendChild(document.createTextNode(listJadlospis[i].dietaKod));
+            tr.appendChild(td);
+
+            var td = document.createElement('TD');
+            td.appendChild(document.createTextNode(listJadlospis[i].dietaNazwa));
+            tr.appendChild(td);
+            
+            var td = document.createElement('TD');
+            td.appendChild(document.createTextNode(listJadlospis[i].dObr));
+            tr.appendChild(td);
+            
+            var td = document.createElement('TD');
+            td.appendChild(document.createTextNode(listJadlospis[i].w1));
+            tr.appendChild(td);
+            
+            var td = document.createElement('TD');
+            td.appendChild(document.createTextNode(listJadlospis[i].w2));
+            tr.appendChild(td);
+            
+            var td = document.createElement('TD');
+            td.appendChild(document.createTextNode(listJadlospis[i].w3));
+            tr.appendChild(td);
+            
+            var td = document.createElement('TD');
+            td.appendChild(document.createTextNode(listJadlospis[i].w4));
+            tr.appendChild(td);
+            
+            var td = document.createElement('TD');
+            td.appendChild(document.createTextNode(listJadlospis[i].w5));
+            tr.appendChild(td);
+            
+            var td = document.createElement('TD');
+            td.appendChild(document.createTextNode(listJadlospis[i].w6));
+            tr.appendChild(td);
+            
+            var td = document.createElement('TD');
+            td.appendChild(document.createTextNode(listJadlospis[i].w7));
+            tr.appendChild(td);
+            
+            var td = document.createElement('TD');
+            td.appendChild(document.createTextNode(listJadlospis[i].w8));
+            tr.appendChild(td);
+            
+            var td = document.createElement('TD');
+            td.appendChild(document.createTextNode(listJadlospis[i].w9));
+            tr.appendChild(td);
+            
+            var td = document.createElement('TD');
+            td.appendChild(document.createTextNode(listJadlospis[i].w10));
+            tr.appendChild(td);
+            
+            var td = document.createElement('TD');
+            td.appendChild(document.createTextNode(listJadlospis[i].w11));
+            tr.appendChild(td);
+            
+            var td = document.createElement('TD');
+            td.appendChild(document.createTextNode(listJadlospis[i].w12));
+            tr.appendChild(td);
+            
+            var td = document.createElement('TD');
+            td.appendChild(document.createTextNode(listJadlospis[i].w13));
+            tr.appendChild(td);
+            
+            var td = document.createElement('TD');
+            td.appendChild(document.createTextNode(listJadlospis[i].w14));
+            tr.appendChild(td);
+            
+            var td = document.createElement('TD');
+            td.appendChild(document.createTextNode(listJadlospis[i].w15));
+            tr.appendChild(td);
+            
+            var td = document.createElement('TD');
+            td.appendChild(document.createTextNode(listJadlospis[i].w16));
+            tr.appendChild(td);
+
+            tableBody.appendChild(tr);
+            
+        }  
+        
+        myTableDiv.appendChild(table);
+
 
         });
 }
